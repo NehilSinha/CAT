6e597135e056385e12c8706ff76484c02508b14e
Anushka Gupta <anushkagupta120103@gmail.com>
2026-09-01 12:55:06 +0530

    Add healthChecker controller with health endpoint

---
+++ src/main/java/com/SRTS/CAT/controller/healthChecker.java
@@ -0,0 +1,16 @@
+package com.SRTS.CAT.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class healthChecker {
+    @GetMapping("/h")
+    public String health() {
+        return "Hello World!";
+    }
+    
+
+}
