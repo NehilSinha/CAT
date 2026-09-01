eebd3681afe6a5aaa5facd4120d36ce1eb25d009
Nehil Sinha <86055271+NehilSinha@users.noreply.github.com>
2026-09-01 13:01:11 +0530

    Changes in the healthChecker

--- src/main/java/com/SRTS/CAT/controller/healthChecker.java
+++ src/main/java/com/SRTS/CAT/controller/healthChecker.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class healthChecker {
     @GetMapping("/h")
     public boolean health() {
-        return true;
+        return false;
     }
     
 
