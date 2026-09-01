78c019357dee725287a08e8f95d5e79f1d8a9f0e
Nehil Sinha <86055271+NehilSinha@users.noreply.github.com>
2026-09-01 13:15:15 +0530

    Pushing Fixes

--- src/main/java/com/SRTS/CAT/entity/EquipmentEntry.java
+++ src/main/java/com/SRTS/CAT/entity/EquipmentEntry.java
@@ -1,20 +1,28 @@
 package com.SRTS.CAT.entity;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
 
 @Data
 public class EquipmentEntry {
-    
+
+ @Id
  private Object id;
 
+
  private String equipmentCode;
+ private String equipmentName;
  private String type;
  private String status;
- private String siteId;
+ private boolean activeState;
+ private ObjectId siteId;
  private String currentLocation;
- private String engineHoursPerDay;
- private String idleHoursPerDay;
- private String operatingDays;
- private String lastOperatorId;
+ private Integer engineHoursPerDay;
+ private Integer idleHoursPerDay;
+ private Integer operatingDays;
+ private ObjectId lastOperatorId;
 
 }
